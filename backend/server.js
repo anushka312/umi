e22881f413f5069e4f899a227041c1573c2936b7
Anushka <anushka031205@gmail.com>
2025-07-14 14:01:33 +0530

    added nft

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 connectDB();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://verdefi.onrender.com/'],
+  origin: ['http://localhost:5173', 'https://verdefi.onrender.com'],
   credentials: true
 }));
 
